5486ab816bfe58a0831ecfc85c4a3b26f77f5112
Sánta Gergely <santagergely90@gmail.com>
2021-12-24 14:47:37 +0100

    Menu system.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,12 +7,28 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField]
     private Image fadeImage;
+    [SerializeField]
+    private GameObject menu;
 
     public static MenuManager instance;
 
     void Start()
     {
         instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (menu.activeInHierarchy)
+            {
+                menu.SetActive(false);
+            } else
+            {
+                menu.SetActive(true);
+            }
+        }
     }
 
     public void FadeImage()
